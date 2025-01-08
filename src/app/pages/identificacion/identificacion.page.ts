e9ebe22b2fa13896db103586998bf3549e7b0afe
Ian Cortes <ia.cortes@duocuc.cl>
2025-01-08 17:33:28 -0300

    formulario "terminado", comienzo de menú desplegable

--- src/app/pages/identificacion/identificacion.page.ts
+++ src/app/pages/identificacion/identificacion.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonText, IonToolbar, IonTitle, IonButton, IonContent, IonItem, IonLabel} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,13 +9,45 @@ import { IonHeader, IonText, IonToolbar, IonTitle, IonButton, IonContent, IonIte
   templateUrl: './identificacion.page.html',
   styleUrls: ['./identificacion.page.scss'],
   standalone: true,
-  imports: [FormsModule, IonText, IonToolbar, IonTitle, IonButton, IonContent, IonItem, IonLabel, IonHeader]
+  imports: [FormsModule, IonicModule]
 })
 export class IdentificacionPage implements OnInit {
 
-  constructor() { }
+  cliente = {
+    nombre: '',
+    apellido: '',
+    correo: '',
+    fechayhora: '',
+  }
 
   ngOnInit() {
   }
+
+  
+
+
+
+  cantidad: number = 1; // Valor inicial
+
+  increment() {
+    this.cantidad++;
+  }
+  
+  decrement() {
+    if (this.cantidad > 1) {
+      this.cantidad--;
+    }
+  }
+
+  constructor(private router: Router) {}
+
+  
+  onSubmit() {
+    if (this.cliente.nombre && this.cliente.apellido && this.cliente.correo && this.cliente.fechayhora) {
+      // Lógica adicional antes de la redirección
+      this.router.navigate(['/menu']); // Cambia '/ruta-destino' por tu ruta deseada
+    }
+  }
+
 
 }
